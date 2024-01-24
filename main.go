804a3624e868710674c623341ce9d2c31cbd3686
Dav He <s991179@gmail.com>
2024-01-25 06:29:51 +0800

    feat: test gorm

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Car struct {
+	Id   int `gorm:"primaryKey"`
+	Name string
+}
 
 func main() {
+	connect_string := "host=localhost user=admin password=1234 dbname=testDb port=5432"
+	db, err := gorm.Open(postgres.Open(connect_string), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Failed!!!!!")
+	}
+
+	var car Car
+
+	db.First(&car)
+
 	fmt.Println("1234")
+	fmt.Println(car)
 }
