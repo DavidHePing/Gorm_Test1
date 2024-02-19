8b6cb5564ff21c4ba8b2fedbf7dcf30881950748
Dav He <s991179@gmail.com>
2024-02-20 07:51:01 +0800

    feat: add before udpate

--- model/car.go
+++ model/car.go
@@ -1,7 +1,18 @@
 package model
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Car struct {
-	Id    int `gorm:"primaryKey"`
-	Name  string
-	Price int
+	Id        int `gorm:"primaryKey"`
+	Name      string
+	Price     int
+	UpdatedAt time.Time
+}
+
+func (car *Car) BeforeUpdate(db *gorm.DB) {
+	car.UpdatedAt = time.Now()
 }
