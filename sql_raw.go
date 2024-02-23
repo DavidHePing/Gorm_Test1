1cd298758a36ac18a913866e2f8b9ede6bd3d46c
Dav He <s991179@gmail.com>
2024-02-23 08:00:03 +0800

    feat: use single variable

--- sql_raw.go
+++ sql_raw.go
@@ -11,4 +11,8 @@ func raw_select_test1(db *gorm.DB) {
 	var cars = []model.Car2{}
 	db.Raw("select * from test_cars where is_deleted is null").Scan(&cars)
 	fmt.Println(cars)
+
+	var car = model.Car2{}
+	db.Raw("select * from test_cars where is_deleted is null").Scan(&car)
+	fmt.Println(car)
 }
