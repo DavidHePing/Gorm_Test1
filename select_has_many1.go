73284df28f57b136de5d6902fe3958cada9d17df
Dav He <s991179@gmail.com>
2024-02-28 07:38:01 +0800

    feat: test preload

--- select_has_many1.go
+++ select_has_many1.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"Gorm_Test1/model"
+	"fmt"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func select_many_test1(db *gorm.DB) {
 	var users []model.User
 	result := db.Find(&users)
-	print(result)
+	fmt.Println(result)
 }
 
 func select_many_test2(db *gorm.DB) {
 	var users []model.User
-	db.Joins("User").Joins("Task").Find(&users)
-	print(users)
+	// db.DryRun = true
+	db.Preload(clause.Associations).Omit("random_number").Find(&users)
+	fmt.Println(users)
 }
